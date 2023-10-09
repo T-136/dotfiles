38304723e7eade1f4e3232fb39115a6baf28a8f6
tilman <tilman.grueger@web.de>
2023-10-09 16:33:53 +0200

    exchanged astronvim with lazynvim

--- nvim/.config/nvim/colors/oceanic_material_p.vim
+++ nvim/.config/nvim/colors/oceanic_material_p.vim
@@ -1169,9 +1169,3 @@ call s:set_color_variables()
 call s:apply_syntax_highlightings()
 
 " vim: set sw=2 ts=2 sts=2 et tw=80 ft=vim fdm=marker fmr={{{,}}}:
-
-
-
-
-
-
---
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  -- add gruvbox
+  -- { "../../colors/oceanic_material_p.vim " },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "oceanic_material_p",
+    },
+  },
+}

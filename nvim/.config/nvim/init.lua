a892ef0908f0c957e796096d410c78e2dedc2885
tilman <t136.git@gmail.com>
2025-01-18 13:01:44 +0100

    nvim color scheme

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,2 +1,32 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+require("gruvbox").setup({
+  overrides = {
+    variable = { bg = "#ff9900" },
+  },
+})
+require("gruvbox").setup({
+  overrides = {
+    ["@variable"] = { link = "Identifier" },
+    ["@keyword"] = { link = "GruvboxPurple" },
+    ["@keyword.conditional"] = { link = "GruvboxPurple" },
+    ["@keyword.debug"] = { link = "GruvboxPurple" },
+    ["@keyword.directive"] = { link = "GruvboxPurple" },
+    ["@keyword.directive.define"] = { link = "GruvboxPurple" },
+    ["@keyword.exception"] = { link = "GruvboxPurple" },
+    ["@keyword.function"] = { link = "GruvboxPurple" },
+    ["@keyword.import"] = { link = "GruvboxPurple" },
+    ["@keyword.operator"] = { link = "GruvboxPurple" },
+    ["@keyword.repeat"] = { link = "GruvboxPurple" },
+    ["@keyword.return"] = { link = "GruvboxPurple" },
+    ["@keyword.storage"] = { link = "GruvboxPurple" },
+    ["@function"] = { link = "GruvboxRed" },
+    ["@function.builtin"] = { link = "GruvboxRed" },
+    ["@function.call"] = { link = "GruvboxRed" },
+    ["@function.macro"] = { link = "GruvboxRed" },
+    ["@function.method"] = { link = "GruvboxRed" },
+    ["@method"] = { link = "GruvboxRed" },
+    ["@method.call"] = { link = "GruvboxRed" },
+  },
+})
+vim.cmd("colorscheme gruvbox")
--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,22 @@
 return {
-  -- add gruvbox
-  -- { "../../colors/oceanic_material_p.vim " },
+  --{ "../../colors/oceanic_material_p.vim " },
 
+  -- { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "oceanic_material_p",
+    },
+  },
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
     },
   },
 }

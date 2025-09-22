ed39aead901e0165b8712a3f11d47269bc9b8ee0
tilman <t136.git@gmail.com>
2025-09-22 10:10:23 +0200

    frecency, preview_scroll

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -3,8 +3,25 @@ return {
   "folke/snacks.nvim",
   opts = {
     picker = {
+      matcher = {
+        frecency = true, -- <- activate frecency scoring
+      },
+      debug = {
+        scores = true,
+      },
       layout = {
         preset = "ivy_split", -- <- valid preset
+      },
+      win = {
+        input = {
+          keys = {
+            -- use H J K L to scroll the preview
+            ["H"] = { "preview_scroll_left", mode = { "n", "i" } },
+            ["L"] = { "preview_scroll_right", mode = { "n", "i" } },
+            ["K"] = { "preview_scroll_up", mode = { "n", "i" } },
+            ["J"] = { "preview_scroll_down", mode = { "n", "i" } },
+          },
+        },
       },
     },
   },
